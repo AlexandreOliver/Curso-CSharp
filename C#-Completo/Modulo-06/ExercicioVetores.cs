74a85823f255505ad85142427bac2320e69bb68d
Alexandre Oliver <Alexandreferreiradeoliveira40@gmail.com>
2022-12-16 15:18:53 -0300

    Exercicio Vetores

---
+++ C#-Completo/Modulo-06/ExercicioVetores.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Globalization;
+
+namespace Mod6
+{
+    class exercicio
+    {
+        static void Main()
+        {
+            Cliente[] quartos = new Cliente[10];
+
+            Console.Write("Quantos quartos serão alugados? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"\nAluguel #{i}:");
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Quarto: ");
+                int quarto = int.Parse(Console.ReadLine());
+
+                quartos[quarto] = new Cliente(nome, email);
+            }
+
+            Console.WriteLine("\nQuartos Ocupados: ");
+            for (int i = 0; i < quartos.Length; i++)
+            {
+                if (quartos[i] != null)
+                    Console.WriteLine($"{i}: {quartos[i]}");
+                
+            }
+
+        }
+    }
+
+    class Cliente
+    {
+        public string Nome {get; set;}
+        public string Email {get; set;}
+
+        public Cliente(string nome, string email)
+        {
+            Nome = nome;
+            Email = email;
+        }
+
+        public override string ToString()
+        {
+            return Nome + ", " + Email;
+        }
+
+
+    }
+}
