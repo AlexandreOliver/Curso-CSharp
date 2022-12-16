838fc5d1a63b8c9fdccfaea8614fe72f5d6fa112
Alexandre Oliver <Alexandreferreiradeoliveira40@gmail.com>
2022-12-16 15:19:16 -0300

    Aula sobre listas

---
+++ C#-Completo/Modulo-06/Listas.cs
@@ -0,0 +1,90 @@
+using System;
+using System.Collections.Generic;
+// Nescessario para usar listas
+
+namespace Mod06
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+        //  List<Tipo> nome = new List<Tipo>()
+        
+            List<string> lista = new List<string>();
+        //   Declaração        e    Instanciamento
+
+            List<string> lista2 = new List<string>
+            {"Maria", "james", "Igor"};
+
+        //  Adicionar Elementos na lista -- Add
+            lista.Add("Alex");
+            lista.Add("Bob");
+            lista.Add("Jonas");
+            lista.Add("Albenes");
+            lista.Add("Matheus");
+            lista.Add("Daniel");
+
+            foreach (string obj in lista)
+            {
+                Console.WriteLine(obj);
+            }
+        //  Adicionar Elementos por indice -- Insert
+            lista.Insert(2, "Marco");
+
+        //  Mostrar o Tamanho da lista -- Count
+            Console.WriteLine("Tamanho da Lista: " + lista.Count);
+
+        //  Pesquisar na lista a primeira ocorrencia -- Find
+            string s1 = lista.Find(x => x[0] == 'A');
+            Console.WriteLine("Primeiro elemento comecando com A: " + s1);
+        //  Pesquisar na lista a ultima ocorrencia -- FindLast
+            s1 = lista.FindLast(x => x[0] == 'A');
+            Console.WriteLine("Ultimo elemento comecando com A: "+ s1);
+        //  Encontrar Primeira Posição -- FindIndex
+            int posi = lista.FindIndex(x => x[0] == 'A');
+            Console.WriteLine("Primeira Posicao 'A' " + posi);
+        //  Encontrar Ultima Posicao -- FindLastIndex
+            posi = lista.FindLastIndex(x => x[0] == 'A');
+            Console.WriteLine("Ultima Posicao 'A' " + posi);
+        //  Filtrar a lista dado um predicado -- FindAll
+            List<string> NewLista = lista.FindAll(x => x.Length == 5);
+            
+            foreach (string obj in NewLista)
+            {
+                Console.WriteLine(obj);
+            }
+
+        //  Remover elemento especifico da lista -- Remove
+            lista.Remove("Alex");
+            
+            foreach (string obj in lista)
+            {
+                Console.WriteLine(obj);
+            }
+        //  Remover elementos com algo em comum -- RemoveAll
+            lista.Remove(x => x[0] == "M");
+
+            foreach (string obj in lista)
+            {
+                Console.WriteLine(obj);
+            }
+        //  Remover elemento pela posicao -- RemoveAt
+            lista.RemoveAt(2);
+
+            foreach (string obj in lista)
+            {
+                Console.WriteLine(obj);
+            }
+        //  Remover elementos de uma faixa -- RemoveRange
+            lista.RemoveRange(2, 2);
+
+            foreach (string obj in lista)
+            {
+                Console.WriteLine(obj);
+            }
+            
+        }
+    }
+
+    
+}
